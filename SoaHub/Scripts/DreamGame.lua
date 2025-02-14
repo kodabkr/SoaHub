f1d89a6da618bad7b00de2dbe258f99190044521
Koda Baker <90579924+kodabkr@users.noreply.github.com>
2025-02-14 01:40:06 -0500

    Upd

--- SoaHub/Scripts/DreamGame.lua
+++ SoaHub/Scripts/DreamGame.lua
@@ -164,7 +164,4 @@ local infSilverToggle = automationTab:CreateToggle({
 })
 
 local StatusLabel = automationTab:CreateLabel("Status: N/A (Toggle is off.)", "")
-
-local paragraphDivider = automationTab:CreateDivider()
-
 local Paragraph = Tab:CreateParagraph({Title = "Bunny Gap", Content = "This toggle will automatically search for and detect a Bunny Gap in Infinite Silver, a 1/6000+ event."})
