ed0af1d2080cfc0047b0383379490da59f9b39d7
Koda Baker <90579924+kodabkr@users.noreply.github.com>
2025-02-14 00:46:56 -0500

    Update

--- SoaHub/Loader/Loader.lua
+++ SoaHub/Loader/Loader.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-local gamesList = loadstring(game:HttpGet("https://raw.githubusercontent.com/kodabkr/gamesList/main/supportedGames"))()
+local gamesList = loadstring(game:HttpGet("https://raw.githubusercontent.com/kodabkr/SoaHub/refs/heads/main/SoaHub/SupportedGames/Games.lua"))()
 
 for i, v in pairs(Games) do
     if i == game.PlaceId then
--- SoaHub/SupportedGames/Games.lua
+++ SoaHub/SupportedGames/Games.lua
@@ -1,8 +1,4 @@
 -- Supported Games
-
 getgenv().Games = {
-    [13230182363] = "https://raw.githubusercontent.com/kodabkr/plus1clicker/main/main.Lua",
-    [13892638440] = "https://raw.githubusercontent.com/kodabkr/pixelIncremental/main/main.lua".
-    [14236123211] = "https://raw.githubusercontent.com/kodabkr/punchSimulator/main/punchSimulator.lua"
-    [11860394049] = "https://raw.githubusercontent.com/kodabkr/lumberLegendsSimulator/main/main.lua"
+    ["gameId as number"] = "string to code for game.",
 }
