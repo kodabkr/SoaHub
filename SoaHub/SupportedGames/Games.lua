07e05f8096415aec290cbfbdd1db981315f83701
Koda Baker <90579924+kodabkr@users.noreply.github.com>
2025-02-16 11:07:10 -0500

    upd

--- SoaHub/SupportedGames/Games.lua
+++ SoaHub/SupportedGames/Games.lua
@@ -1,5 +1,7 @@
 -- Supported Games
+
 getgenv().Games = {
     [5475056496] = "https://raw.githubusercontent.com/kodabkr/SoaHub/refs/heads/main/SoaHub/Scripts/DreamGame.lua",
-    [8304191830] = ""
+    [12886143095] = "https://raw.githubusercontent.com/kodabkr/SoaHub/refs/heads/main/SoaHub/Scripts/AnimeLastStand.lua",
+    [8304191830] = "https://raw.githubusercontent.com/kodabkr/SoaHub/refs/heads/main/SoaHub/Scripts/AnimeAdventures.lua",
 }
